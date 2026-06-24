808ee8353858ce69a8f825ebdc5b0e943b4d0b0c
Piotr Wegrzynek <pwe@trifork.com>
2026-06-24 10:17:28 +0200

    Revert "Merge pull request #16 from trifork/feature/message-archiving-conditional-contact-processing-hook"
    This reverts commit e8aaefd3bd8c90f633f081249891fd9e3c6b7dac, reversing
    changes made to 52a2cccdebe0a2784932be0eaf9d27ffe3c5f264.

--- Extensions/XEP-0136/CoreDataStorage/XMPPMessageArchivingCoreDataStorage.m
+++ Extensions/XEP-0136/CoreDataStorage/XMPPMessageArchivingCoreDataStorage.m
@@ -143,12 +143,6 @@ static XMPPMessageArchivingCoreDataStorage *sharedInstance;
 	// Override hook
 }
 
-- (BOOL)shouldProcessContactForMessage:(XMPPMessageArchiving_Message_CoreDataObject *)message
-{
-    // Override hook
-    return YES;
-}
-
 - (void)willInsertContact:(XMPPMessageArchiving_Contact_CoreDataObject *)contact
 {
 	// Override hook
@@ -157,6 +151,12 @@ static XMPPMessageArchivingCoreDataStorage *sharedInstance;
 - (void)didUpdateContact:(XMPPMessageArchiving_Contact_CoreDataObject *)contact
 {
 	// Override hook
+}
+
+- (BOOL)shouldInsertContactForMessage:(XMPPMessageArchiving_Message_CoreDataObject *)message
+{
+	// Override hook
+	return YES;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -508,14 +508,14 @@ static XMPPMessageArchivingCoreDataStorage *sharedInstance;
 			
 			// Create or update contact (if message with actual content)
 			
-			if ([self messageContainsRelevantContent:message] && [self shouldProcessContactForMessage:archivedMessage])
+			if ([self messageContainsRelevantContent:message])
 			{
 				BOOL didCreateNewContact = NO;
 				
 				XMPPMessageArchiving_Contact_CoreDataObject *contact = [self contactForMessage:archivedMessage];
 				XMPPLogVerbose(@"Previous contact: %@", contact);
 				
-				if (contact == nil)
+				if (contact == nil && [self shouldInsertContactForMessage:archivedMessage])
 				{
 					contact = (XMPPMessageArchiving_Contact_CoreDataObject *)
 					    [[NSManagedObject alloc] initWithEntity:[self contactEntity:moc]
@@ -523,6 +523,11 @@ static XMPPMessageArchivingCoreDataStorage *sharedInstance;
 					
 					didCreateNewContact = YES;
 				}
+                
+                if (contact == nil) {
+                    XMPPLogVerbose(@"No contact for archived message");
+                    return;
+                }
 				
 				contact.streamBareJidStr = archivedMessage.streamBareJidStr;
 				contact.bareJid = archivedMessage.bareJid;
