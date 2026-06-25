6397270c50a0f0c0c087903bc6483654f8a30049
Wojciech Nagrodzki <wna@trifork.com>
2026-06-25 17:26:03 +0200

    Make sure OMEMOModule handles error and result responses only (inspired by other modules e.g. XMPPRoom)

--- Extensions/OMEMO/OMEMOModule.m
+++ Extensions/OMEMO/OMEMOModule.m
@@ -381,16 +381,19 @@ static const int xmppLogLevel = XMPP_LOG_LEVEL_WARN;
     }
 }
 
-- (BOOL)xmppStream:(XMPPStream *)sender didReceiveIQ:(XMPPIQ *)iq {
-    BOOL success = NO;
-    if (!iq.from) {
-        // Some error responses for self or contacts don't have a "from"
-        success = [self.tracker invokeForID:iq.elementID withObject:iq];
-    } else {
-        success = [self.tracker invokeForElement:iq withObject:iq];
+- (BOOL)xmppStream:(XMPPStream *)sender didReceiveIQ:(XMPPIQ *)iq {    
+    NSString *type = [iq type];
+    
+    if ([type isEqualToString:@"result"] || [type isEqualToString:@"error"]) {
+        if (!iq.from) {
+            // Some error responses for self or contacts don't have a "from"
+            return [self.tracker invokeForID:iq.elementID withObject:iq];
+        } else {
+            return [self.tracker invokeForElement:iq withObject:iq];
+        }
     }
     //DDLogWarn(@"Could not match IQ: %@", iq);
-    return success;
+    return NO;
 }
 
 #pragma mark XMPPCapabilitiesDelegate methods
